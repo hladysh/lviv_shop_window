1862122b0318e4fe42a99086c5da40af4ace50be
Ivan <hladysh.im@gmail.com>
2017-01-03 12:56:18 +0000

    done thirt part

---
+++ scripts/main.js
@@ -0,0 +1,48 @@
+    $(document).ready(function() {
+      $('.slider_top').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        autoplay: true,
+        arrows: false
+
+      });
+    });
+
+
+
+    $('.slider_offer').slick({
+      infinite: true,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      }, {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }, {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }]
+    });
+
+
+
+    $('.nav_slide_button').click(function() {
+      $('.pull').slideToggle();
+    });
+    $("#nav-toggle").click(function() {
+      $(this).toggleClass("active");
+    });
+    
