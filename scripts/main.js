2b09201518b0fea1ccc3ec6db038b4c6a6473692
Ivan <hladysh.im@gmail.com>
2017-01-12 13:35:47 +0000

    done

--- scripts/main.js
+++ scripts/main.js
@@ -4,36 +4,36 @@
         infinite: true,
         speed: 300,
         slidesToShow: 1,
-        autoplay: true,
+        // autoplay: true,
         arrows: false
 
       });
 
 
-      $('.slider_offer').slick({
-        infinite: true,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        responsive: [{
-          breakpoint: 1199,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3
-          }
-        }, {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-          }
-        }, {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }]
-      });
+    $('.slider_offer').slick({
+      infinite: true,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      responsive: [{
+        breakpoint: 1199,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      }, {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }, {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }]
+    });
 
 
 
@@ -45,7 +45,7 @@
         infinite: true,
         speed: 500,
         slidesToShow: 1,
-        autoplay: true,
+        // autoplay: true,
         arrows: true
 
       });
@@ -56,14 +56,52 @@
         slidesToShow: 1,
         arrows: true
       });
+      
 
-
-
-      $('.nav_slide_button').click(function() {
-        $('.pull').slideToggle();
+      $('.slider_advice_news').slick({
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+        arrows: false
       });
-      $("#nav-toggle").click(function() {
-        $(this).toggleClass("active");
+
+
+       $('.slider_partners').slick({
+        dots: false,
+        infinite: true,
+        speed: 800,
+        slidesToShow: 1,
+        arrows: true
       });
+
+
+    $('.nav_slide_button').click(function() {
+      $('.pull').slideToggle();
     });
-    
+    $("#nav-toggle").click(function() {
+      $(this).toggleClass("active");
+    });
+
+
+    initialize();
+});
+
+	function initialize() {
+    var haightAshbury = new google.maps.LatLng(49.8340064,23.9780386);//(долгота, широта)
+    var mapOptions = {
+        zoom: 15,//масштаб
+        center: haightAshbury,//позиционируем карту на заданые координаты
+        // mapTypeId: google.maps.MapTypeId.TERRAIN,//задаем тип карты
+        scrollwheel: false,
+	    navigationControl: false,
+	    mapTypeControl: false,
+	    scaleControl: false,
+	    // draggable: false
+    };    
+    map = new google.maps.Map(document.getElementById("map"), mapOptions);//инициализация карты
+ 
+    google.maps.event.addListener(map, 'click', function (event) {
+        addMarker(event.latLng);
+    });//добавляем событие нажание мышки
+}
